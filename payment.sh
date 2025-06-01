07b08188838161c287832ca10bcbea2ac993844f
harshatejaadduri <harshateja3@gmail.com>
2025-06-01 17:49:02 +0530

    Updated

--- payment.sh
+++ payment.sh
@@ -50,7 +50,7 @@ echo "User already exists"
 fi
 VALID $? "Adding User" 
 
-mkdir /app  &>>$log_file
+mkdir -p /app  &>>$log_file
 VALID $? "Creating app directory"
 
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip  &>>$log_file
